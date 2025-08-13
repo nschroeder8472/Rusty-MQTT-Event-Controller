a6c43c36c728bd13e705c02c80ea7ef1801bd765
Nik <nschroeder8472@gmail.com>
2025-08-12 20:32:32 -0400

    in-progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::time::Duration;
+use rumqttc::{AsyncClient, LastWill, MqttOptions};
+use tokio::task;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut mqttoptions = MqttOptions::new("rusty-scene-controller", "10.56.18.200", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+
+    task::spawn(async move {
+        req
+    })
+}
+
+async fn requests(client: AsyncClient) {
+    client.subscribe()
 }
